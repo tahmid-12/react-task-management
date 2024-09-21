521e90b80df2f910de8a2eaab508a435bbb34cb8
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-09-21 21:05:06 +0600

    packages Created and Pushed to Git

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children:[
+    //   {
+    //     path: "/",
+    //     element: <AllDrawings />
+    //   },
+    //   {
+    //     path: "/singledrawing",
+    //     element: <SingleDrawing />
+    //   }
+    // ]
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
